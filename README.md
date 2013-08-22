b8f12643b5dbc76d7dd7dc940f3670b1cec2d2a7
Wei Jin <jinw@delmar.ca>
2013-08-22 09:56:52 -0400

    create project structure

---
+++ README.md
@@ -0,0 +1,4 @@
+spring-mvc-api
+========================
+
+Sample Spring MVC Application demonstrating JSON AJAX requests with jQuery front-end and Spring MVC back-end.
---
+++ src/main/java/ca/delmar/api/springconfig/WebConfig.java
@@ -0,0 +1,42 @@
+package ca.delmar.api.springconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="ca.delmar.api")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/view/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+    @Bean
+    public DataSource stagingOracleLookup() {
+        try {
+            InitialContext initialContext = new InitialContext();
+            return (DataSource) initialContext.lookup("OracleDS");
+        } catch (NamingException e) {
+            throw new RuntimeException("JNDI lookup failed.",e);
+        }
+    }
+}
